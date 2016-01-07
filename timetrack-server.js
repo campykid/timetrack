20f0f1e6d6be133ee194323134233d2050e42638
Vladislav Khvostov <vladkhvo@gmail.com>
2016-01-07 09:28:37 +0300

    0.0.1

--- timetrack-server.js
+++ timetrack-server.js
@@ -1,38 +1,38 @@
 var http = require('http');
-// var work = require('./lib/timetrack');
+var work = require('./lib/timetrack');
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-	host: '127.0.0.1',
-	user: 'root',
+	host:		 '127.0.0.1',
+	user:		 'root',
 	password: '123',
 	database: 'timetrack'
-})
+});
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
 	switch (req.method) {
-		case 'POST':
+		case 'POST': 
 			switch(req.url) {
 				case '/':
-				work.show(db, req, res);
-				break;
-			case '/archive':
-				work.archive(db, req, res);
-				break;
-			case '/delete':
-				work.delete(db, req, res);
-				break;
+					work.add(db, req, res);
+					break;
+				case '/archive':
+					work.archive(db, req, res);
+					break;
+				case '/delete':
+					work.delete(db, req, res);
+					break;
 			}
 			break;
-		case 'GET':
-		switch(req.url) {
-			case '/':
-			work.show(db, res);
+		case 'GET': 
+			switch(req.url) {
+				case '/':
+					work.show(db, res);
+					break;
+				case '/archived':
+					work.showArchived(db, res);
+			}
 			break;
-		case '/archived':
-			work.showArchived(db, res);
-		}
-	break;
 	}
 });
 
@@ -44,12 +44,9 @@ db.query(
 	+ "archived INT(1) DEFAULT 0, " 
 	+ "description LONGTEXT,"
 	+ "PRIMARY KEY(id))",
-	function(err) {
+	function(err) { 
 		if (err) throw err;
 		console.log('Server started...');
 		server.listen(3000, '127.0.0.1'); 
 	}
 );
-
-
-
